74b7c83a9373da33b9c21d2a1bfb3138e9a98126
TA2k <tombox2020@gmail.com>
2021-08-21 22:00:53 +0200

    finish device update

--- main.js
+++ main.js
@@ -52,8 +52,10 @@ class ZehnderCloud extends utils.Adapter {
         this.json2iob = new Json2iob(this);
 
         await this.login();
+
         if (this.session.id_token) {
             await this.getDeviceList();
+            this.updateDevices();
             this.updateInterval = setInterval(async () => {
                 await this.updateDevices();
             }, this.config.interval * 60 * 1000);
@@ -63,7 +65,7 @@ class ZehnderCloud extends utils.Adapter {
         }
     }
     async login() {
-        const [code_verifier, codeChallenge] = this.getCodeChallenge();
+        let [code_verifier, codeChallenge] = this.getCodeChallenge();
         const headers = {
             "User-Agent": "ioBroker 1.0",
         };
@@ -72,7 +74,7 @@ class ZehnderCloud extends utils.Adapter {
             url:
                 "https://zehndergroupauth.b2clogin.com/zehndergroupauth.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_signin_signup_enduser&client_id=df77b1ce-c368-4f7f-b0e6-c1406ac6bac9&nonce=" +
                 this.randomString(16) +
-                "&redirect_uri=https%3A%2F%2Flocalhost%2Fmyweb&scope=openid&response_type=code&prompt=login&code_challenge=" +
+                "&redirect_uri=https%3A%2F%2Flocalhost%2Fmyweb&scope=openid%20offline_access&response_type=code&prompt=login&code_challenge=" +
                 codeChallenge +
                 "&code_challenge_method=S256",
 
@@ -94,8 +96,8 @@ class ZehnderCloud extends utils.Adapter {
             return;
         }
 
-        const csrf = htmlLoginForm.split('"csrf":"')[1].split('"')[0];
-        const state = htmlLoginForm.split("StateProperties=")[1].split('"')[0];
+        let csrf = htmlLoginForm.split('"csrf":"')[1].split('"')[0];
+        let state = htmlLoginForm.split("StateProperties=")[1].split('"')[0];
         let data = "request_type=RESPONSE&email=" + encodeURIComponent(this.config.username) + "&password=" + encodeURIComponent(this.config.password);
         headers["X-CSRF-TOKEN"] = csrf;
         await this.requestClient({
@@ -150,10 +152,10 @@ class ZehnderCloud extends utils.Adapter {
             grant_type: "authorization_code",
             code: code,
             redirect_uri: "https://localhost/myweb",
-            scope: "openid",
+            scope: "openid offline_access",
             code_verifier: code_verifier,
         };
-
+        this.log.debug(JSON.stringify(data));
         await this.requestClient({
             method: "post",
             url: "https://zehndergroupauth.b2clogin.com/zehndergroupauth.onmicrosoft.com/B2C_1_signin_signup_enduser/oauth2/v2.0/token",
@@ -190,33 +192,47 @@ class ZehnderCloud extends utils.Adapter {
         })
             .then(async (res) => {
                 this.log.debug(JSON.stringify(res.data));
-                for (const device of res.data.data) {
-                    this.idArray.push(device.id);
-                    await this.setObjectNotExistsAsync(device.id, {
-                        type: "device",
-                        common: {
-                            name: device.modelId,
-                        },
-                        native: {},
-                    });
-
-                    await this.setObjectNotExistsAsync(device.id + ".general", {
-                        type: "channel",
-                        common: {
-                            name: "General Device Information",
-                        },
-                        native: {},
-                    });
-
-                    this.json2iob.parse(device.id + ".general", device);
-                }
+                this.idArray = res.data;
             })
             .catch((error) => {
                 this.log.error(error);
                 error.response && this.log.error(JSON.stringify(error.response.data));
             });
     }
-    async updateDevices() {}
+    async updateDevices() {
+        const headers = {
+            "Content-Type": "application/json",
+            Accept: "*/*",
+            "User-Agent": "ioBroker 1.0.0",
+            Authorization: "Bearer " + this.session.id_token,
+            "x-api-key": this.config.subKey,
+        };
+        for (let id of this.idArray) {
+            id = id.toString();
+            await this.requestClient({
+                method: "get",
+                url: "https://zehnder-prod-we-apim.azure-api.net/cloud/api/v2.1/devices/byid/" + id + "/details",
+                headers: headers,
+            })
+                .then(async (res) => {
+                    this.log.debug(JSON.stringify(res.data));
+                    const device = res.data;
+                    await this.setObjectNotExistsAsync(id, {
+                        type: "device",
+                        common: {
+                            name: device.assistantName || device.description,
+                        },
+                        native: {},
+                    });
+
+                    this.json2iob.parse(id, device);
+                })
+                .catch((error) => {
+                    this.log.error(error);
+                    error.response && this.log.error(JSON.stringify(error.response.data));
+                });
+        }
+    }
     async refreshToken() {
         await this.requestClient({
             method: "post",
@@ -285,13 +301,31 @@ class ZehnderCloud extends utils.Adapter {
      * @param {string} id
      * @param {ioBroker.State | null | undefined} state
      */
-    onStateChange(id, state) {
+    async onStateChange(id, state) {
         if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
+            if (!state.ack) {
+                const vidin = id.split(".")[2];
+
+                let command = id.split(".")[4];
+                const action = command.split("-")[1];
+                command = command.split("-")[0];
+
+                this.refreshTimeout = setTimeout(async () => {
+                    await this.updateDevices();
+                }, 10 * 1000);
+            } else {
+                // const resultDict = { chargingStatus: "CHARGE_NOW", doorLockState: "DOOR_LOCK" };
+                // const idArray = id.split(".");
+                // const stateName = idArray[idArray.length - 1];
+                const vin = id.split(".")[2];
+                // if (resultDict[stateName]) {
+                //     let value = true;
+                //     if (!state.val || state.val === "INVALID" || state.val === "NOT_CHARGING" || state.val === "ERROR" || state.val === "UNLOCKED") {
+                //         value = false;
+                //     }
+                //     await this.setStateAsync(vin + ".remote." + resultDict[stateName], value, true);
+                // }
+            }
         }
     }
 }
